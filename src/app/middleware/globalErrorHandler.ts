cd99aef884f6147f74105b19b81789f74fa5029c
HabiburRahmanAnik <117148549+habib22666@users.noreply.github.com>
2023-07-14 00:23:26 +0600

    add global error handler with mongoose validation and zod validation

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,26 +1,62 @@
-import { ErrorRequestHandler } from "express";
-import { ErrorMessage } from "../../types/error";
-import mongoose from "mongoose";
-import {ZodError} from 'zod'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ErrorRequestHandler } from 'express';
+import { ErrorMessage } from '../../types/error';
+import { Error } from 'mongoose';
+import { ZodError } from 'zod';
 import { StatusCodes } from 'http-status-codes';
+import handleMongooseValidationError from '../../errors/handleMongooseValidationError';
+import handleZodValidationError from '../../errors/handleZodValidationError';
+import ApiError from '../../errors/apiError';
+import config from '../../config';
 
-const globalErrorHandler:ErrorRequestHandler = (err, req, res, next) => {
-    let status=500;
-    let message="Something went wrong!";
-    let errorMessages:ErrorMessage =[];
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorMessages: ErrorMessage[] = [];
 
-    if(err instanceof mongoose.Error.ValidationError){
+  if (err instanceof Error.ValidationError) {
+    const simplifiedError = handleMongooseValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errors;
+  } else if (err instanceof Error.CastError) {
+    statusCode = StatusCodes.NOT_ACCEPTABLE;
+    message = 'Cast Error';
+    errorMessages = [{ path: '', message: `Invalid Id` }];
+  } else if (err instanceof ZodError) {
+    const simplifiedError = handleZodValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errors;
+  } else if (err instanceof ApiError) {
+    statusCode = err.statusCode;
+    message = err.message;
+    errorMessages = err.message
+      ? [
+          {
+            path: '',
+            message: err.message,
+          },
+        ]
+      : [];
+  } else if (err instanceof Error) {
+    message = err.message;
+    errorMessages = err.message
+      ? [
+          {
+            path: '',
+            message: err.message,
+          },
+        ]
+      : [];
+  }
 
-    }else if(err instanceof mongoose.Error.CastError){
-
-    }else if(err instanceof ZodError){
-
-    }else if (err.code === 11000) {
-        // Handle MongoDB duplicate key error
-        statusCode = StatusCodes.CONFLICT;
-        message = 'Duplicate key error';
-        errorMessages = [{ path: '', message: 'Duplicate key found' }];
-      }
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
+  });
 };
 
 export default globalErrorHandler;
